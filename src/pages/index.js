8f131df9dcea3fe6a0919586acc1e32160807e88
Cernan Bernardo <cernanb@gmail.com>
2022-08-06 14:46:46 -0700

    Remove amplify

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,15 @@ import { useUser } from "../context/AuthContext";
 
 export default function Home() {
   const { user } = useUser();
+  console.log(user);
   return (
     <>
       <h1>Home</h1>
-      {user ? <div>You are logged in</div> : <div>You are not logged in</div>}
+      {user ? (
+        <div>You are logged in as {user.attributes.email}</div>
+      ) : (
+        <div>You are not logged in</div>
+      )}
     </>
   );
 }
